0f8593c3e19591c92d42319913a1545e8034759b
David Tanderup <Davetanderup@gmail.com>
2019-02-25 18:01:17 -0500

    Add project files.

---
+++ Exercise 2A - Calculating Averages/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2A___Calculating_Averages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
