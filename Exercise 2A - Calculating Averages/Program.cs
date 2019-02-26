5f2d79eb7a5b8cabeb127c59cb627ddf67433b5c
David Tanderup <Davetanderup@gmail.com>
2019-02-25 20:10:54 -0500

    Create the addition method

--- Exercise 2A - Calculating Averages/Program.cs
+++ Exercise 2A - Calculating Averages/Program.cs
@@ -10,6 +10,50 @@ namespace Exercise_2A___Calculating_Averages
     {
         static void Main(string[] args)
         {
+            Addition();
+            
         }
+        
+        static void Addition()
+        {            
+            int countDown = 10;
+            int countUp = 1;
+            double total = 0;
+            do
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("You may enter any number between 0 - 100");
+                Console.Write($"\nPlease enter #{countUp}: ");
+                double input = double.Parse(Console.ReadLine());
+                // catches any entries made outside the expected parameters. 
+                //This does not catch errors where the user enters text or hit a wrong key
+                if (input > 100 || input <0)
+                {
+                    while (input > 100 || input < 0)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("That number is out of range.");
+                        Console.WriteLine("You may enter any number between 0 - 100");
+                        Console.Write($"\nPlease re-enter #{countUp}: ");
+                        input = double.Parse(Console.ReadLine());
+                    }                                            
+                }
+                
+                countDown--;
+                countUp++;
+                total = input + total;
+                
+
+            } while (countDown > 0);
+            Console.Clear();
+            Console.WriteLine("SUCCESS!!!");
+            Console.WriteLine($"\nYour total is {total}! Your contribution really added" +
+                $" up!");
+            Console.ReadLine();
+        }
+        
+        
     }
 }
